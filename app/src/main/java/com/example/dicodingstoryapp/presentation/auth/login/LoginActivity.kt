799599165644fdc244cd903af7a9995662fb8a75
andikadeveloper <andikarastafara93@gmail.com>
2022-08-24 22:24:58 +0700

    feature: slicing login screen

--- app/src/main/java/com/example/dicodingstoryapp/presentation/auth/login/LoginActivity.kt
+++ app/src/main/java/com/example/dicodingstoryapp/presentation/auth/login/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.example.dicodingstoryapp.presentation.auth.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dicodingstoryapp.R
+import com.example.dicodingstoryapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
