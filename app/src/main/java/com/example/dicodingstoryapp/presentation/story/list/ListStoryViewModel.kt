4cbe0f145af2a859b305fcb1063c7666544ab154
andikadeveloper <andikarastafara93@gmail.com>
2022-08-24 10:10:06 +0700

    chore: extend ListStoryViewModel into ViewModel class

--- app/src/main/java/com/example/dicodingstoryapp/presentation/story/list/ListStoryViewModel.kt
+++ app/src/main/java/com/example/dicodingstoryapp/presentation/story/list/ListStoryViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.dicodingstoryapp.presentation.story.list
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.dicodingstoryapp.domain.usecase.StoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ListStoryViewModel @Inject constructor(storyUseCase: StoryUseCase) {
+class ListStoryViewModel @Inject constructor(storyUseCase: StoryUseCase): ViewModel() {
     val stories = storyUseCase.getAllStory().asLiveData()
 }
